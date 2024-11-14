8677fdbc7bd5280d6904760a093459f81152ec3b
Bilge Su Kuzucular <bilgesukuzucular@gmail.com>
2024-11-14 15:23:26 +0300

    Add project files.

---
+++ PatikaCodeFirst/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatikaCodeFirst.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
---
+++ PatikaCodeFirst/PatikaFirstDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatikaCodeFirst.Models;
+
+namespace PatikaCodeFirst
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=PatikaCodeFirstDb1;Trusted_Connection=True;");
+        }
+    }
+}
